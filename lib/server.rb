0bad71000d9d82f16bc44e60f4788957bc0bea93
Parker P <exxcaelum@gmail.com>
2016-04-14 19:30:15 -0600

    Removing Psuedo code

--- lib/server.rb
+++ lib/server.rb
@@ -21,7 +21,6 @@ class Server
       output = response.output(request, counter, @ps)
       headers = ["HTTP/1.1 302 Found",
             "Location: http://127.0.0.1:9297/game\r\n\r\n"].join("\r\n")
-
       @ps.info = client.read(response.content_length(request))
       client.puts headers if request.verb == "POST" && @ps.game == true && request.path == "/game"
       client.puts output
@@ -40,22 +39,3 @@ class Server
     request_lines
   end
 end
-
-
-  # def get_response
-  #   # response = <pre> + response.output(request, client, counter) + </pre>
-  #   # Body, Headers, Verb, Path, Protocol
-  #   @output = "<html><head></head><body><pre>#{response}</pre></body></html>"
-  #   @headers = ["http/1.1 200 ok",
-  #             "date: #{Time.now.strftime('%a, %e %b %Y %H:%M%S %z')}",
-  #             "server: ruby",
-  #             "Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
-  #             "content-type: text/html; charset=iso-8859-1",
-  #             "content-length: #th}\r\n\r\n#{@output}"].join("\r\n")
-  # end
-  #
-  # def display_message(client)
-  #   puts ["Wrote this response:", @headers, @output].join("\n")
-  #   client.puts @headers
-  #   client.puts @output
-  # end
